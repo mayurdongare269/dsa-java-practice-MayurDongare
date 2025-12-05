2b2ca6b2bcdcf88d8d29f3cf9f79d8691d0c16ac
Mayur Dongare <mayurdongare269@gmail.com>
2025-12-05 12:36:22 +0530

    _3432_Count_Partitions_with_Even_Sum_Difference.. added 1 pass

--- Arrays/_3432_Count_Partitions_with_Even_Sum_Difference.java
+++ Arrays/_3432_Count_Partitions_with_Even_Sum_Difference.java
@@ -8,26 +8,37 @@ public class _3432_Count_Partitions_with_Even_Sum_Difference {
     }
 }
 
-class Solution {// done ,.... two pass solution ...
+class Solution {// done ,.... 
     public int countPartitions(int[] nums) {
-        int count = 0;
+        // one pass solution(need some maths...)
         int n = nums.length;
+        int sum = sumf(nums, 0, n-1);
+        if(sum % 2 == 0) {
+            return n - 1;
+        }
+        return 0;
+
+
+
+        // // two pass solution ...
+        // int count = 0;
+        // int n = nums.length;
         
-        int totSum = 0;
-        for(int i = 0; i < n; i++) {
-            totSum += nums[i];
-        }
+        // int totSum = 0;
+        // for(int i = 0; i < n; i++) {
+        //     totSum += nums[i];
+        // }
 
-        for(int i = 0; i <= n-2; i++) {
-            int prev = sumf(nums, 0, i); // leftsum == prev
-            int next = totSum - prev; // rightsum == next
-            int ans = prev - next;
-            if(ans % 2 == 0) {
-                count++;
-            }
-        }
+        // for(int i = 0; i <= n-2; i++) {
+        //     int prev = sumf(nums, 0, i);
+        //     int next = totSum - prev;
+        //     int ans = prev - next;
+        //     if(ans % 2 == 0) {
+        //         count++;
+        //     }
+        // }
 
-        return count;
+        // return count;
     }
 
     public int sumf(int[] arr, int s, int e) {
@@ -39,3 +50,36 @@ class Solution {// done ,.... two pass solution ...
         return sum;
     }
 }
+
+
+// class Solution {// done ,.... two pass solution ...
+//     public int countPartitions(int[] nums) {
+//         int count = 0;
+//         int n = nums.length;
+        
+//         int totSum = 0;
+//         for(int i = 0; i < n; i++) {
+//             totSum += nums[i];
+//         }
+
+//         for(int i = 0; i <= n-2; i++) {
+//             int prev = sumf(nums, 0, i); // leftsum == prev
+//             int next = totSum - prev; // rightsum == next
+//             int ans = prev - next;
+//             if(ans % 2 == 0) {
+//                 count++;
+//             }
+//         }
+
+//         return count;
+//     }
+
+//     public int sumf(int[] arr, int s, int e) {
+//         int sum = 0;
+//         for(int i = s; i <= e; i++) {
+//             sum += arr[i];
+//         }
+
+//         return sum;
+//     }
+// }
